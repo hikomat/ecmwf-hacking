e93401a54794ea41cf70a372d3c1bf659311f2c3
ngivo <nagy.gabor.i@gmail.com>
2016-12-07 15:51:13 +0100

    Added concated directory generation

--- concat.py
+++ concat.py
@@ -1,12 +1,21 @@
 import pandas as pd
 import glob
+import os
 
-for directory in glob.glob('data/*'):
-    print(directory)
-    files = sorted(glob.glob(directory + '/*'))
-    ds = []
-    for fi in files:
-        print(fi)
-        ds.append(pd.read_csv(fi))
-    d = pd.concat(ds)
-    d.to_csv('concated/{}.csv'.format(directory.replace('/', '.')))
+
+if __name__ == '__main__':
+
+    try:
+        os.mkdir('concated')
+    except:
+        pass
+
+    for directory in glob.glob('data/*'):
+        print(directory)
+        files = sorted(glob.glob(directory + '/*'))
+        ds = []
+        for fi in files:
+            print(fi)
+            ds.append(pd.read_csv(fi))
+        d = pd.concat(ds)
+        d.to_csv('concated/{}.csv'.format(directory.replace('/', '.')))
